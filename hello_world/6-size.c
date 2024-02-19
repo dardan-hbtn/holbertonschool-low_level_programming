a6f6eef2acdcb8690430de0a8d3dcb704dcaf71b
dardan-hbtn <8760@holbertonstudents.com>
2024-02-19 12:40:41 +0000

    file 54

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,10 +7,11 @@
  */
 int main(void)
 {
-printf("Size of a char: 1 byte(s)" \n)
-printf("Size of an int: 4 byte(s)" \n)
-printf("Size of a long int: 4 byte(s)" \n)
-printf("Size of a long long int: 8 byte(s)" \n)
-printf("Size of a float: 4 byte(s)" \n)
+	int number = 4;
+printf("Size of a char: %ld byte(s)\n", sizeof(char));
+printf("Size of an int: %li byte(s) \n", sizeof(int));
+printf("Size of a long int: %li byte(s) \n", sizeof(int));
+printf("Size of a long long int: %li byte(s) \n", sizeof(int));
+printf("Size of a float: %ld byte(s) \n", sizeof(float));
 return (0);
 }
