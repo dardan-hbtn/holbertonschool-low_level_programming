574f3217876b78e9f49896fe0138e8e0d5ef48ac
dardan-hbtn <8760@holbertonstudents.com>
2024-02-19 12:48:25 +0000

    file 54

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	int number = 4;
+
 printf("Size of a char:%ld byte(s)\n", sizeof(char));
 printf("Size of an int:%li byte(s)\n", sizeof(int));
 printf("Size of a long int:%li byte(s)\n", sizeof(long int));
