4c59cdcbf6f10037c797109a1e78dcafab8565e4
dardan-hbtn <8760@holbertonstudents.com>
2024-02-20 12:31:55 +0000

    changes

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+#include<stdlib.h>
+/**
+* main - main function
+* Return: 0 value
+*/
+int main(void)
+{
+char alphabet = 'a';
+while (alphabet <= 'z')
+{
+putchar(alphabet);
+alphabet++;
+}
+putchar('\n');
+return (0);
+}
