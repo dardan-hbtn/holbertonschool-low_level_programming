4f0b40afe97dc36cff96bc834f31ea0886eddf47
dardan-hbtn <8760@holbertonstudents.com>
2024-02-21 08:53:53 +0000

    file 7

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include<stdlib.h>
+/**
+* main - main function
+* Return: 0 value
+*/
+int main (void)
+{
+int num;
+
+    for (num = 0; num <= 15; num++)
+    {
+        putchar(num);
+    }
+putchar('\n');
+
+
+}
