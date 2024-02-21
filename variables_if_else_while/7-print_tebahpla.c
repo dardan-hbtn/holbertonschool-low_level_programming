b6695e0987c3abcdcf37ec415abba2e4530206ff
dardan-hbtn <8760@holbertonstudents.com>
2024-02-21 08:42:29 +0000

    file 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+#include<stdlib.h>
+/**
+* main - main function
+* Return: 0 value
+*/
+int main (void)
+{
+char i;
+	for(i='Z';i>='A';i--)
+	{
+		printf("%c",i);
+	}
+    putchar('\n');
+	return 0;
+}
