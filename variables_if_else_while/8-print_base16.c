cab234963937a37a563fdd9b307d2f536c7580c3
dardan-hbtn <8760@holbertonstudents.com>
2024-02-21 09:45:00 +0000

    file 9

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -11,12 +11,12 @@ char n;
 char t;
 for(n = '0'; n <= '9'; n++)
 {
-    putchar(n);
-    }
+putchar(n);
+}
 for(t = 'a'; t <= 'f'; t++ )
 {
-    putchar(t);
-    }
-    putchar('\n');
+putchar(t);
+}
+putchar('\n');
 return(0);
 }
