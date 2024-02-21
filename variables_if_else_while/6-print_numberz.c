be26e667ebbb66b6494bada7ea6bcb6e8611434b
dardan-hbtn <8760@holbertonstudents.com>
2024-02-21 08:21:03 +0000

    file 4

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+#include<stdlib.h>
+/**
+* main - main function
+* Return: 0 value
+*/
+int main (void)
+{
+int i;
+for (i = 10; i < 0; i--)
+{
+printf("%d", i);
+}
+printf("\n");
+return (0);
+}
