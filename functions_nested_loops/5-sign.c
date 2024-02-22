26040552aafe296976d6d48f5970f55235ef9143
dardan-hbtn <8760@holbertonstudents.com>
2024-02-22 20:32:06 +0000

    file 5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,11 +1,22 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
+#include "main.h"
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
+* print_sign- Prints the sing of a number
+* @n: the number to be checked
+* Return: 1 for positive num, -1 for negative num ore 0 for anything else
 */
-int main(void)
+int print_sign(int n)
+{
+ if (n > 0)
+{
+_putchar (43);
+return (1);
+}
+else if (n < 0)
+{_putchar (45);
+return (-1);
+}
+else
+{_putchar (48);
+return (0);
+}
+}
