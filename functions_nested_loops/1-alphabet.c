3e699bb955eeba022048a7c7ab01f0e3d261acfc
dardan-hbtn <8760@holbertonstudents.com>
2024-02-22 11:58:25 +0000

    test

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -5,10 +5,10 @@
 */
 void print_alphabet(void)
 {
-    int c;
-    for(c = 97; c <= 122; c++)
-    {
-        _putchar(c);
-    }
-    _putchar('\n');
+int c;
+for(c = 97; c <= 122; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
 }
