c9e77aba8f984b9f4a30a7aa8f78934235e6a86f
dardan-hbtn <8760@holbertonstudents.com>
2024-02-22 10:04:04 +0000

    file

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+    int n;
+
+    for (n = 0; n < 10; n++) {
+        putchar (n + '0');
+        
+    }
+    putchar ('\n');
+    return (0);
+}
+
+
+
+
