b3f4057cbbfbc77b69032049d8aa099ab48a90e2
dardan-hbtn <8760@holbertonstudents.com>
2024-02-23 08:23:57 +0000

    file 6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,15 +1,16 @@
 #include "main.h"
-/**_abs - Computes the absolut value of an integer.
-*@c: The number to be computes.
-*Returne: Absolut value of a number ore zero.
-*/
-int _abs(int)
+ /**
+ * _abs - Computes the absolute value of an integer.
+ * @c: The number to be computed.
+ * Return: Absolut value of a number ore zero
+ */
+ int _abs(int c)
 {
 if (c < 0)
 {
 int abs_val;
 abs_val = c * -1;
 return (abs_val);
- }
+}
 return (c);
 }
