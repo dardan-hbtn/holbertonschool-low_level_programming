d58aabaf8d153cdc3577f09a779a153e50a11cab
dardan-hbtn <8760@holbertonstudents.com>
2024-02-27 12:43:33 +0000

    file 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*swap - swap the value of the two integers
+*@a: value of first number
+*@b: value of secend number
+*/
+void swap_int(int *a, int *b)
+{
+    int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+
+}
