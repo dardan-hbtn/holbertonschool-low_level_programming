c74da44394d45f9f68b41a529203b07708315e43
dardan-hbtn <8760@holbertonstudents.com>
2024-02-29 10:19:58 +0000

    file 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _puts - prints a string
+ * @s: the string to print
+ * Return:void
+ */
+void _puts(char *str)
+
+{
+   for(; *str != '\0'; str++)
+     {
+        _putchar(*str);
+     }
+    _putchar('\n');
+
+}
