e58ccae9b0822b0026c547b0494d9b69253c4cd1
dardan-hbtn <8760@holbertonstudents.com>
2024-03-04 08:28:33 +0000

    file

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -7,14 +7,14 @@
  * @size: input
  * Return: NULL.
  */
-void print_diagsums(int *a, int size)
+void print_array(int *a, int n)
 {
-	int i, n, sum1 = 0, sum2 = 0;
+	int i, sum1 = 0, sum2 = 0;
 
-	for (i = 0; i <= (size * size); i = i + size + 1)
+	for (i = 0; i <= (sizeof(a) / sizeof(int)); i = i + size + 1)
 		sum1 = sum1 + a[i];
 
-	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
+	for (n = size - 1; n <= (sizeof(a) / sizeof(int)) - size; n = n + size - 1)
 	sum2 = sum2 + a[n];
 	printf("%d, %d\n", sum1, sum2);
 }
