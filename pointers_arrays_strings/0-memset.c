667d485e5e0918e87e4e9d21790d6b5dd911a154
dardan-hbtn <8760@holbertonstudents.com>
2024-03-05 08:35:42 +0000

    file 0 1

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*_memset - Every point
+*@s: pointed destination
+*@b: constant byte
+*@n: bytes
+*Return: Always 0 (Seccess)
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+   for(i = 0; i < n; i++)
+       s[i] = b;
+    return (s);
+}
