074cfa821d8e1844de62a4a39b0bbcd2b865d387
dardan-hbtn <8760@holbertonstudents.com>
2024-03-06 12:49:38 +0000

    file 1

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,17 +1,19 @@
 #include "main.h"
 #include "stdio.h"
 /**
-*_puts_recursion - function that prints a string
-*@s: string
-*Return: NULL.
-*/
+ * _puts_recursion - Prints a string followed by a new line
+ * @s: string
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 void _puts_recursion(char *s)
 {
-if (*s == '\0')
-_putchar ('\n');
-else
-{
-_putchar (*s);
-_puts_recursion (s + 1);
-}
+	if (*s == 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,14 +1,16 @@
 #include "main.h"
 /**
-*_print_rev_recursion - function that prints a string in reverse
-*@c: string in reverse
-*Return: NULL.
-*/
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: string
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 void _print_rev_recursion(char *s)
 {
-if (*s != '\0')
-{
-_print_rev_recursion (s + 1);
-_putchar (*s);
-}
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
