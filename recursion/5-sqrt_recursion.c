3ace3496b8d6a1d31e8b644812730ee724aebb86
dardan-hbtn <8760@holbertonstudents.com>
2024-03-07 11:44:51 +0000

    file

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _sqrt_recursion -  returns the natural square root of a number
+* @n: Number Integer
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+
+int _sqrt_recursion(int n)
+{
+
+
+    
+}
---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: Number Integer
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int is_prime_number(int n)
+{
+
+
+
+    
+}
