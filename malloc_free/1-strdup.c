d5e8895455a1db71b5bc158633e4e2c59586e6a6
dardan-hbtn <8760@holbertonstudents.com>
2024-03-12 10:34:41 +0000

    file 2

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -12,14 +12,14 @@ char *_strdup(char *str)
 int len, i;
 char *dup;
 if(str == NULL)
-return(NULL);
-len =0;
+return (NULL);
+len = 0;
 while (str[len] != '\0')
 len++;
 dup = malloc(sizeof(char) * (len + 1));
 if (dup == NULL)
-return NULL;
-for(i = 0; str[i] != '\0'; i++)
+return (NULL);
+for (i = 0; str[i] != '\0'; i++)
 dup[i] = str[i];
 return (dup);    
 }
