ef0f6fd62c61fe8b5356580a8e6015904206adbb
dardan-hbtn <8760@holbertonstudents.com>
2024-03-12 10:37:04 +0000

    file 2

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -11,11 +11,11 @@ char *create_array(unsigned int size, char c)
 {
 unsigned int i;
 char *array = malloc(sizeof(char) * size);
-if(array == NULL || size == 0)
-return(NULL);
-for(i = 0; i < size; i++)
+if (array == NULL || size == 0)
+return (NULL);
+for (i = 0; i < size; i++)
 {
-array[i]= c;
+array [i]= c;
 }
-return(array);
+return (array);
 }
